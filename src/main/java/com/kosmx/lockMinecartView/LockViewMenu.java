93b6833eb7aa9643de3c4bc3643a8706246f4391
KosmX <kosmx.mc@gmail.com>
2020-06-23 22:37:38 +0200

    1.16 update

--- src/main/java/com/kosmx/lockMinecartView/LockViewMenu.java
+++ src/main/java/com/kosmx/lockMinecartView/LockViewMenu.java
@@ -3,8 +3,11 @@ package com.kosmx.lockMinecartView;
 import java.util.Optional;
 import java.util.function.Supplier;
 
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.gui.ClothConfigScreen;
 import net.minecraft.client.gui.screen.Screen;
 
 public class LockViewMenu implements ModMenuApi{
@@ -14,9 +17,9 @@ public class LockViewMenu implements ModMenuApi{
         return "lock_minecart_view";
     }
 
+
     @Override
-    public Optional<Supplier<Screen>> getConfigScreen(Screen screen){
-        return Optional.of(AutoConfig.getConfigScreen(LockViewConfig.class, screen));
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return screen -> AutoConfig.getConfigScreen(LockViewConfig.class, screen).get();
     }
-    
 }
